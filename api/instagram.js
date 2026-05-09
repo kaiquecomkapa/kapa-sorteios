db217b97780b3c5e8dbbca811c0d72516d735db6
Kaique Com Kapa <kaiquecomkapa@gmail.com>
2026-05-09 08:05:35 +0100

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -2,22 +2,21 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://i.instagram.com/api/v1/users/web_profile_info/?username=kaiquecomkapa", {
       headers: {
-        "User-Agent": "Instagram 155.0.0.37.107"
+        "User-Agent": "Instagram 155.0.0.37.107",
+        "Accept": "application/json"
       }
     });
 
-    const data = await response.json();
-
-    const followers =
-      data.data.user.edge_followed_by.count - 1;
+    const text = await response.text();
 
     res.status(200).json({
-      seguidores: followers
+      status: response.status,
+      resposta: text.slice(0, 500)
     });
 
   } catch (error) {
     res.status(500).json({
-      error: "Erro ao buscar seguidores"
+      error: error.message
     });
   }
 }
